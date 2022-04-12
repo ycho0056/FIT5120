3553c615b5d18b1e20a7397da29b52e06645e472
ycho0056 <ycho0056@student.monash.edu>
2022-04-12 13:34:21 +0800

    code
    add to food list-Jquery

--- Search-bar.php
+++ Search-bar.php
@@ -87,19 +87,17 @@ jQuery(function($){
             var heading = "提示";
         }
         $.toast({
-            text: text,//消息提示框的内容。
-            heading: heading,//消息提示框的标题。
-            icon: icon,//消息提示框的图标样式。
-            showHideTransition: 'fade',//消息提示框的动画效果。可取值：plain，fade，slide。
-            allowToastClose: true,//是否显示关闭按钮。(true 显示，false 不显示)
-            hideAfter: hideAfter,//设置为false则消息提示框不自动关闭.设置为一个数值则在指定的毫秒之后自动关闭消息提框
-            stack: 1,//消息栈。同时允许的提示框数量
-            position: 'top-center',//消息提示框的位置：bottom-left, bottom-right,bottom-center,top-left,top-right,top-center,mid-center。
-            textAlign: 'left',//文本对齐：left, right, center。
-            loader: true,//是否显示加载条
-			//bgColor: '#FF1356',//背景颜色。
-			//textColor: '#eee',//文字颜色。
-            loaderBg: '#ffffff',//加载条的背景颜色。
+            text: text,
+            heading: heading,
+            icon: icon,
+            showHideTransition: 'fade',
+            allowToastClose: true,
+            hideAfter: hideAfter,
+            stack: 1,
+            position: 'top-center',
+            textAlign: 'left',
+            loader: true,
+            loaderBg: '#ffffff',
         });
     }
 });
